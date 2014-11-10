7eece5e5de3d23d72e8a071745b864ef47fa24ba
Ikuo Terado <eqobar@gmail.com>
2014-11-11 00:17:43 +0900

    Add helper for alert with bootstap class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def bootstrap_class_for(level)
+    case level
+      when "success" then "alert-success"
+      when "notice"  then "alert-info"
+      when "alert"   then "alert-warning"
+      when "error"   then "alert-danger"
+    end
+  end
+
 end
